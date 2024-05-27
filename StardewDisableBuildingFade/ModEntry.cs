e2473eb14967fe234170ba98c8f354dee6863b6f
Caleb Stein <caleb@calebstein.net>
2024-05-27 09:16:35 -0700

    add harmony

--- StardewDisableBuildingFade/ModEntry.cs
+++ StardewDisableBuildingFade/ModEntry.cs
@@ -1,3 +1,4 @@
+using HarmonyLib;
 using StardewModdingAPI;
 
 namespace StardewDisableBuildingFade;
@@ -5,6 +6,16 @@ namespace StardewDisableBuildingFade;
 internal sealed class ModEntry : Mod
 {
     public override void Entry(IModHelper helper)
+    {
+        var harmony = new Harmony(ModManifest.UniqueID);
+        
+        harmony.Patch(
+            original: AccessTools.Constructor(typeof(StardewValley.GameData.Buildings.BuildingData)),
+            postfix: new HarmonyMethod(typeof(ModEntry), nameof(DisableFadePrefix))
+        );
+    }
+
+    private void DisableFadePrefix()
     {
         
     }
