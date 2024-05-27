ba7fecd674ea15a4cda0bbd89ac4c008159c39ec
Caleb Stein <caleb@calebstein.net>
2024-05-27 09:02:29 -0700

    initial commit with boilerplate

---
+++ StardewDisableBuildingFade/ModEntry.cs
@@ -0,0 +1,11 @@
+using StardewModdingAPI;
+
+namespace StardewDisableBuildingFade;
+
+internal sealed class ModEntry : Mod
+{
+    public override void Entry(IModHelper helper)
+    {
+        
+    }
+}
